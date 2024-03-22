822eee548dac969dee9a911a39f019fe93152fda
Mumtahin Farabi <mfarabi619@gmail.com>
2024-03-21 20:12:17 -0400

    chore(Find Your Hat): Print field

---
+++ Find Your Hat/main.js
@@ -0,0 +1,26 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+  constructor(field) {
+    this.field = field;
+  }
+
+  print() {
+    this.field.forEach((row) => {
+      console.log(row.join(""));
+    });
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+myField.print();
